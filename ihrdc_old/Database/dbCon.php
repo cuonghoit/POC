4952602cbfd62b50af1f5ac42bd74aa476f50ca3
Thanh Tran <thanhtv@forixwebdesign.com>
2020-01-06 14:14:21 +0700

    [Chickenchild] Firth commit: - Backup the old source. - Commit new laravel 6 source - Integrate the feature basic login/logout, create new user.

--- ihrdc_old/Database/dbCon.php
+++ ihrdc_old/Database/dbCon.php
@@ -3,7 +3,7 @@
 
 
 function getConnection() {
-	$con = mysqli_connect("localhost", "root", "", "ihrdc");
+	$con = mysqli_connect("localhost", "root", "toilatoi", "IHRDC");
 	//mysql_select_db("ihrdc");
 	mysqli_query($con, "SET NAMES 'utf8'");
 	return $con;
