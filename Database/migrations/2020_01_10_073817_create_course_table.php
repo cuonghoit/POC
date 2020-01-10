131f2791214d3c4100adf5ed22ad3da02f3f5cf7
= <=>
2020-01-10 15:32:34 +0700

    add 2 migrate course andd training_record

---
+++ Database/migrations/2020_01_10_073817_create_course_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('course_name');
+            $table->string('discipline');
+            $table->string('course_type');
+            $table->string('course_duration');
+            $table->string('course_objectives');
+            $table->string('course_outline');
+            $table->string('provider');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course');
+    }
+}
