55008fde7168ac978a12198df42d55cbe792b7f5
thanhtungld <hothanhtungld@gmail.com>
2020-02-21 16:45:24 +0700

    [Thanh Tung] add data

--- Database/seeds/courseTableSeeder.php
+++ Database/seeds/courseTableSeeder.php
@@ -11,17 +11,27 @@ class courseTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {        
         DB::table('course')->insert(
-      	[
-       	 'course_name'=>'Project Manager Professional',
-       	 'discipline'=>'3 year working for company',
-       	 'course_type'=>'PMP',
-       	 'course_duration'=>'3 month',
-       	 'course_objectives'=>'Know About PMP',
-       	 'course_outline'=>'N/A',
-       	 'provider'=>'PMI'
-
+        [
+         'course_name'=>'Certificated Scrum Master',
+         'discipline'=>'1 year working for company',
+         'course_type'=>'CSM',
+         'course_duration'=>'1 month',
+         'course_objectives'=>'Know About PMP',
+         'course_outline'=>'N/A',
+         'provider'=>'Scrum.org'
+       ]);
+        DB::table('course')->insert(
+        [
+         'course_name'=>'Project Manager Professional',
+         'discipline'=>'3 year working for company',
+         'course_type'=>'PMP',
+         'course_duration'=>'3 month',
+         'course_objectives'=>'Know About PMP',
+         'course_outline'=>'N/A',
+         'provider'=>'PMI'
        ]);
     }
+
 }
--- Database/seeds/training_recordTableSeeder.php
+++ Database/seeds/training_recordTableSeeder.php
@@ -12,7 +12,7 @@ class training_recordTableSeeder extends Seeder
     public function run()
     {
         DB::table('training_record')->insert(
-        	['user_id'=>'10',
+        	['user_id'=>'1',
         	 'course_id'=>'1',
         	 'training_purpose'=>'Career Development',
         	 'training_type'=>'master',
@@ -31,6 +31,52 @@ class training_recordTableSeeder extends Seeder
         	 'training_cost_estimation_number'=>'20000',
         	 'training_cost_estimation_date'=>'2020-01-10',
         	 'training_tost_for_reFund'=>'10000'
+
+
+        ]);
+        DB::table('training_record')->insert(
+            ['user_id'=>'7',
+             'course_id'=>'2',
+             'training_purpose'=>'Career Development',
+             'training_type'=>'master',
+             'training_time_from'=>'2020-02-21',
+             'training_time_to'=>'2020-03-01',
+             'training_location'=>'New York',
+             'course_fee'=>'15000',
+             'traveling_cost'=>'1000',
+             'accommodation_cost'=>'3000',
+             'training_approval_status'=>'1',
+             'training_progress'=>'Not started',
+             'assigned_by'=>'PHU QUOC PETROLEUM OPERATING COMPANY',
+             'training_budget_resources'=>'',
+             'training_assignment_number'=>'2020-01-10',
+             'training_assignment_date'=>'2020-01-10',
+             'training_cost_estimation_number'=>'20000',
+             'training_cost_estimation_date'=>'2020-01-10',
+             'training_tost_for_reFund'=>'10000'
+
+
+        ]);
+        DB::table('training_record')->insert(
+            ['user_id'=>'8',
+             'course_id'=>'3',
+             'training_purpose'=>'Know About Scrum',
+             'training_type'=>'master',
+             'training_time_from'=>'2020-02-21',
+             'training_time_to'=>'2020-03-01',
+             'training_location'=>'New York',
+             'course_fee'=>'15000',
+             'traveling_cost'=>'1000',
+             'accommodation_cost'=>'3000',
+             'training_approval_status'=>'1',
+             'training_progress'=>'Not started',
+             'assigned_by'=>'PHU QUOC PETROLEUM OPERATING COMPANY',
+             'training_budget_resources'=>'',
+             'training_assignment_number'=>'2020-01-10',
+             'training_assignment_date'=>'2020-01-10',
+             'training_cost_estimation_number'=>'20000',
+             'training_cost_estimation_date'=>'2020-01-10',
+             'training_tost_for_reFund'=>'10000'
 
 
         ]);
