560294ad62374d21af08505976e798880faf7551
Thanh Tran <thanhtv@forixwebdesign.com>
2020-03-25 12:36:32 +0700

    [Chickenchild] Load list department on company page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,7 @@
                     <a href="#submenu1" data-toggle="collapse" aria-expanded="false" class="bg-dark list-group-item list-group-item-action flex-column align-items-start">
                         <div class="d-flex w-100 justify-content-start align-items-center">
                             <span class="fa fa-dashboard fa-fw mr-3"></span>
-                            <span class="menu-collapsed d-none">{{ __('Perfomance Management') }}</span>
+                            <span class="menu-collapsed d-none">{{ __('Performance Management') }}</span>
                             <span class="submenu-icon ml-auto d-none"></span>
                         </div>
                     </a>
